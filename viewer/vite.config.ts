09f3d2dc9ae5bb20ab5c2324de7bf93442835125
Edward Kotarski <edktrsk@gmail.com>
2025-11-08 09:15:05 +0000

    improve pwa dev config

--- viewer/vite.config.ts
+++ viewer/vite.config.ts
@@ -3,11 +3,6 @@
  * Copyright (c) 2023-2025 Noodle-Bytes. All Rights Reserved
  */
 
-/*
- * SPDX-License-Identifier: MIT
- * Copyright (c) 2023-2024 Vypercore. All Rights Reserved
- */
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 // Plugin that synchronises vite's path searching with tsconfig settings.
@@ -15,57 +10,79 @@ import tsconfigPaths from "vite-tsconfig-paths";
 // Plugin to allow download as a Progressive Web Application (PWA)
 import { VitePWA } from 'vite-plugin-pwa'
 
+
+const getPluginPWA = ((env) => {
+    const manifest = {
+        name: 'Bucket',
+        short_name: 'Bucket',
+        description: 'Buckets of coverage!',
+        theme_color: '#ffffff',
+        file_handlers: [
+            {
+                action: '/',
+                accept: {
+                    "application/json": [".json"],
+                }
+            }
+        ],
+        icons: [
+            {
+                src: '/pwa-192x192.png',
+                sizes: '192x192',
+                type: 'image/png'
+            },
+            {
+                src: '/pwa-512x512.png',
+                sizes: '512x512',
+                type: 'image/png'
+            },
+        ],
+        // TODO: Installation splash screen
+        // screenshots : [
+        //     {
+        //         src: "screenshot-wide.png",
+        //         sizes: "1920x1080",
+        //         type: "image/png",
+        //         form_factor: "wide"
+        //     },
+        //     {
+        //         src: "screenshot-narrow.png",
+        //         sizes: "1080x1920",
+        //         type: "image/png",
+        //         form_factor: "narrow"
+        //     }
+        // ]
+    };
+
+    const includeAssets = [
+        '/favicon.ico',
+        '/apple-touch-icon-180x180.png',
+        '/maskable-icon-512x512.png'
+    ];
+
+    const devConfig = {};
+
+    if (env.mode === 'development') {
+        devConfig['devOptions'] = {
+            enabled: true,
+            type: 'module'
+        };
+    }
+
+    return VitePWA({
+        registerType: 'autoUpdate',
+        includeAssets,
+        manifest,
+        ...devConfig
+    })
+})
+
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig((env) => {
+    return {
         plugins: [react(),
-                  tsconfigPaths(),
-                  VitePWA({
-                    registerType: 'autoUpdate',
-                    includeAssets: [
-                        '/favicon.ico',
-                        '/apple-touch-icon-180x180.png',
-                        '/maskable-icon-512x512.png'
-                    ],
-                    manifest: {
-                        name: 'Bucket',
-                        short_name: 'Bucket',
-                        description: 'Buckets of coverage!',
-                        theme_color: '#ffffff',
-                        file_handlers: [
-                            {
-                                action: '/',
-                                accept: {
-                                    "application/json": [".json"],
-                                }
-                            }
-                        ],
-                        icons: [
-                            {
-                                src: '/pwa-192x192.png',
-                                sizes: '192x192',
-                                type: 'image/png'
-                            },
-                            {
-                                src: '/pwa-512x512.png',
-                                sizes: '512x512',
-                                type: 'image/png'
-                            },
-                        ],
-                        // TODO: Installation splash screen
-                        // screenshots : [
-                        //     {
-                        //         src: "screenshot-wide.png",
-                        //         sizes: "1920x1080",
-                        //         type: "image/png",
-                        //         form_factor: "wide"
-                        //     },
-                        //     {
-                        //         src: "screenshot-narrow.png",
-                        //         sizes: "1080x1920",
-                        //         type: "image/png",
-                        //         form_factor: "narrow"
-                        //     }
-                        // ]
-                    }
-                  })],
+        tsconfigPaths(),
+        getPluginPWA(env)
+        ],
+    }
 });
