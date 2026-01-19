81d23b342fba76601ce286b5726da632f83bca57
Stuart Alldred <stuartalldred@gmail.com>
2026-01-19 15:08:51 +0000

    fix: Update copyright year in archive.py from 2023-2025 to 2023-2026

--- bucket/rw/archive.py
+++ bucket/rw/archive.py
@@ -1,5 +1,5 @@
 # SPDX-License-Identifier: MIT
-# Copyright (c) 2023-2025 Noodle-Bytes. All Rights Reserved
+# Copyright (c) 2023-2026 Noodle-Bytes. All Rights Reserved
 
 import csv
 import tarfile
